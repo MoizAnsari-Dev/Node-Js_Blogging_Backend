5a060e38e12544b47889a789c86f1c86dd6a2e8b
Moiz Ansari <moizansari.india@gmail.com>
2025-05-30 19:16:48 +0530

    Likes apis created

--- controllers/postController.js
+++ controllers/postController.js
@@ -31,15 +31,31 @@ export const GetPost = async (req, res) => {
     try {
         const posts = await postModel.find({status: "draft"}).populate('auther', "firstName",)
         if(!posts) return res.json({message: "No post found"})
-        
-        console.log(...isPost);
-        
-        
+                
         res.json({
             GetPost: posts
         })
     } catch (error) {
         res.status(401).json({
+            message: error.message
+        })
+    }
+}
+
+export const LikePost = async (req, res) => {
+    try {
+        const post = await postModel.findById(req.params.id);
+        if (!post) return res.json({message: 'No like posts found'});
+
+        if(post.likes.includes(req.user.id)) {
+            return res.status(400).json({message: 'Post already liked'})
+        }
+
+        post.likes.push(req.user.id)
+        await post.save();
+        res.json(post.likes)
+    } catch (error) {
+        res.status(500).json({
             message: error.message
         })
     }

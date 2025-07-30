6f41642f41836370c8c08a6e46308d5b82b9998e
Moiz Ansari <moizansari.india@gmail.com>
2025-07-30 15:27:47 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,8 +8,6 @@ export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017
 export const JWT_SECRET = process.env.JWT_SECRET || "amanansari"
 
 
-
-
 export const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	limit: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
